a1fe3e62c8dbc9aa28a530721aa94212a7a4e3fe
ysh <thinkingaboutshu@waifu.club>
2021-12-04 14:04:06 +0100

    Add selca2rss.py

---
+++ selca2rss.py
@@ -0,0 +1,113 @@
+#!/usr/bin/python
+# selca2rss.py - Takes selca.kastden.org owner or group url as first argument.
+# Copyright (C) 2021  ysh <thinkingaboutshu@waifu.club>
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Affero General Public License as published
+# by the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU Affero General Public License for more details.
+#
+# You should have received a copy of the GNU Affero General Public License
+# along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+import sys
+import random
+import string
+import requests
+import json
+from html import escape
+
+url = 'https://selca.kastden.org/json/'
+
+args = sys.argv[1].split('/')
+type_ = args[3]
+owner = args[4]
+
+boundary = '----Boundary' \
+         + ''.join(random.sample(string.ascii_letters + string.digits, 16))
+
+headers = dict()
+headers['content-type'] = 'multipart/form-data; boundary=' + boundary
+
+data = """
+Content-Disposition: form-data; name="page_type"\n
+{0}
+--{2}
+Content-Disposition: form-data; name="group"\n
+{1}
+--{2}
+Content-Disposition: form-data; name="limit"\n
+100
+--{2}
+Content-Disposition: form-data; name="time_col"\n
+added_at
+--{2}
+Content-Disposition: form-data; name="ephemeral"\n
+yes
+--{2}--
+""".format(type_, owner, boundary)
+
+resp = requests.post(url, headers=headers, data=data)
+data_dict = json.loads(resp.text)
+
+# assemble dict of a list of entries with post_id as key
+posts = dict()
+for entries in data_dict['entries']:
+    post_id = entries['post']['post_id']
+    posts.setdefault(post_id, []).append(entries)
+
+feed_description = feed_title = owner + ' ' + type_ + ' selca'
+print("""<?xml version="1.0" encoding="UTF-8"?>
+<rss version="2.0">
+<channel>
+<title>{0}</title>
+<description>{1}</description>
+<link>https://selca.kastden.org/{2}/{3}/</link>
+""".format(feed_title, feed_description, type_, owner))
+
+for post_id in posts:
+    entries = posts[post_id]
+    text = entries[0]['post']['text']
+    if text is None:
+        text = '-'
+    display_name = entries[0]['owner']['display_name']
+    service = entries[0]['post']['service']
+    if entries[0]['post']['ephemeral']:
+        ephemeral = 'story'
+    else:
+        ephemeral = 'post'
+    title = """{0} {1} {2}: {3}""".format(
+            display_name, service, ephemeral, text.split('\n')[0])
+    guid = link = 'https://selca.kastden.org/post/' + str(post_id)
+    date = entries[0]['post']['created_at']
+    description = text
+    print("""
+    <item>
+    <title>{0}</title>
+    <link>{1}</link>
+    <guid>{2}</guid>
+    <pubDate>{3}</pubDate>
+    <description>{4}
+    <![CDATA[
+    """.format(escape(title), link, guid, date, escape(description)))
+
+    for entry in entries:
+        filename = entry['media']['filename']
+        media_id = entry['media']['media_id']
+        original = """https://selca.kastden.org/original/{0}/{1}""".format(
+                media_id, filename)
+        print('<img src="' + original + '"/>')
+
+    print("""
+    ]]>
+    </description>
+    </item>""")
+
+print("""
+</channel>
+</rss>""")
