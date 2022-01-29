4adc880b186cb8cfc24b9ebbf58a5615218dbbd9
ysh <thinkingaboutshu@waifu.club>
2022-01-29 15:15:56 +0100

    Add -t flag specifying the tags to match against

--- re-filter.py
+++ re-filter.py
@@ -20,27 +20,34 @@ import sys
 import re
 from xml.etree.ElementTree import ElementTree
 
+def matches(item, tags, re):
+    for t in tags:
+        tag = item.find(t)
+        if tag is not None:
+            if bool(regex.search(tag.text)):
+                return True
+    return False
+
 p = argparse.ArgumentParser()
 p.add_argument('pattern')
 p.add_argument("-i", action='store_true', help="Ignore case distinctions.")
 p.add_argument("-v", action='store_true', help="Ignore matching items.")
+p.add_argument("-t", action='store', nargs='*', help="Tags to match against.")
 args = p.parse_args(sys.argv[1:])
 
 flags = re.IGNORECASE if args.i else 0
 regex = re.compile(args.pattern, flags)
+
+if args.t is None:
+    args.t = ['title', 'description']
 
 tree = ElementTree()
 tree.parse(sys.stdin)
 
 for channel in tree.findall('channel'):
     for item in tree.findall('.//item'):
-        title = item.find('title')
-        description = item.find('description')
-        if title is not None:
-            match = bool(regex.search(title.text))
-            if not match and description is not None:
-                match = bool(regex.search(description.text))
-            if match ^ (not args.v):
-                channel.remove(item)
+        match = matches(item, args.t, regex)
+        if match ^ (not args.v):
+            channel.remove(item)
 
 tree.write(sys.stdout, encoding='unicode')
