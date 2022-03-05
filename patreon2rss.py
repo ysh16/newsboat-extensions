72264ad5c9636f266a396c1d0222193f61396044
ysh <thinkingaboutshu@waifu.club>
2022-03-06 00:47:30 +0100

    Add patreon2rss.py

---
+++ patreon2rss.py
@@ -0,0 +1,59 @@
+#!/usr/bin/python
+# patreon2rss.py
+# Copyright (C) 2022  ysh <thinkingaboutshu@waifu.club>
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Affero General Public License as published
+# by the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU Affero General Public License for more details.
+#
+# You should have received a copy of the GNU Affero General Public License
+# along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+import sys
+import requests
+import json
+from html import escape
+
+campaign_id = sys.argv[1]
+url = 'https://www.patreon.com/api/posts?filter[campaign_id]=' + campaign_id + "&sort=-published_at&json-api-version=1.0"
+
+resp = requests.get(url)
+data = json.loads(resp.text)
+
+feed_title = data['included'][0]['attributes']['vanity']
+feed_description = data['included'][1]['attributes']['summary']
+
+print("""<?xml version="1.0" encoding="UTF-8"?>
+<rss version="2.0">
+<channel>
+<title>{0}</title>
+<description>{1}</description>
+<link>https://www.patreon.com/{2}</link>"""
+    .format(feed_title, feed_description, feed_title))
+
+for post in data['data']:
+    title = post['attributes']['title']
+    link = post['attributes']['patreon_url']
+    date = post['attributes']['edited_at']
+    description = post['attributes']['teaser_text']
+    if description is None:
+        description = ''
+
+    print("""
+    <item>
+    <title>{0}</title>
+    <link>{1}</link>
+    <pubDate>{2}</pubDate>
+    <description>{3}</description>
+    </item>"""
+        .format(escape(title), link, date, escape(description)))
+
+print("""
+</channel>
+</rss>""")
